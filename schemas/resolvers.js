30260d82dcc25664145bb85ab18926a69ee5bc82
FionaN <fiona1941nicdao@gmail.com>
2022-03-14 21:20:02 -0500

    mutations for add user and profile

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -30,8 +30,10 @@ const resolvers = {
     },
     // Query all the profiles except for the logged in user.  
     // Have the algorithm for matches
-    // Needs to be by gender interests and attachment style
+    // Needs to be by gender interests and attachment style and locations 25miles 
+    // error handleing -if person is too far a way have something written 
     // Math.random
+    // 
   },
   Mutation: {
     login: async (parent, { email, password }) => {
@@ -50,6 +52,36 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    addUser:async (parent, { email, password }) => {
+      const user = await User.create({ email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addProfile: async (parent, {profileData}, context)=>{
+      if(context.user){
+        const profile = await Profile.create({profileData},
+          {_id: context.user._id},
+          // {$addToSet: {Profile: profileData}},
+          {new: true},
+        );
+        return profile;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // updateProfile: async
+    removeThread:async (parent, {threadId}, context) => {
+      if (context.user) {
+        const deleteThread = await Thread.findOneAndUpdate(
+          { _id: context.user._id },
+          {$pull:{threadId}},
+          {new:true}
+        );
+        // delete the messages too ! 
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // removeMessage
   },
 };
 
