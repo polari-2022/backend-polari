a7d42067d262d25ce549a1a86d0fc86b757b07d5
Jonathan Parker <jprkr21@gmail.com>
2022-03-15 20:27:48 -0500

    adding user query in typedefs

--- models/Message.js
+++ models/Message.js
@@ -3,15 +3,46 @@ const { Schema, model } = require('mongoose');
 const messageSchema = new Schema({
     text: {
         type: String,
-        required: true,
+        require: true,
     },
     date:{
         type:Date,
-        required:true,
+        require:true,
         default:Date.now,
     }, 
+    thread:{
+        type:Schema.Types.ObjectId,
+        ref:'Thread'
+    },
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    }
 })
 
 const Message = model('Message', messageSchema);
 
 module.exports = Message;
+
+
+
+
+
+
+// const { Schema, model } = require('mongoose');
+
+// const messageSchema = new Schema({
+//     text: {
+//         type: String,
+//         required: true,
+//     },
+//     date:{
+//         type:Date,
+//         required:true,
+//         default:Date.now,
+//     }, 
+// })
+
+// const Message = model('Message', messageSchema);
+
+// module.exports = Message;
--- models/Thread.js
+++ models/Thread.js
@@ -3,15 +3,52 @@ const { Schema, model } = require('mongoose');
 const threadSchema = new Schema({
     text:{
        type: String,
-       required:true, 
+       require:true, 
     },
     date:{
         type:Date,
-        required:true,
+        require:true,
         default:Date.now,
     },
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    match:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    messages:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Message'
+        }
+    ]
 })
 
 const Thread = model('Thread', threadSchema);
 
 module.exports = Thread;
+
+
+
+
+
+
+// const { Schema, model } = require('mongoose');
+
+// const threadSchema = new Schema({
+//     text:{
+//        type: String,
+//        required:true, 
+//     },
+//     date:{
+//         type:Date,
+//         required:true,
+//         default:Date.now,
+//     },
+// })
+
+// const Thread = model('Thread', threadSchema);
+
+// module.exports = Thread;
