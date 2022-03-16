86a42989a981c75098c11c2fe6eddcda9d930c37
FionaN <fiona1941nicdao@gmail.com>
2022-03-15 21:54:01 -0500

    thank BLEH you are the error handeling king

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,22 @@
 const db = require('../config/connection');
-const { Tech } = require('../models');
+const { Message, Profile, Thread, User } = require('../models')
 
-const techData = require('./techData.json');
+const messageData = require('./messageData.json');
+const profileData = require('./profileData.json');
+const threadData = require('./threadData.json');
+const userData = require('./userData.json');
 
-db.once('open', async () => {
-  await Tech.deleteMany({});
+db.once('open', async () =>{
+    await Message.deleteMany({});
+    await Profile.deleteMany({});
+    await Thread.deleteMany({});
+    await User.deleteMany({});
 
-  const technologies = await Tech.insertMany(techData);
+    const messages = await Message.insertMany(messageData);
+    const profiles = await Profile.insertMany(profileData);
+    const threads = await Thread.insertMany(threadData);
+    const users = await User.insertMany(userData);
 
-  console.log('Technologies seeded!');
-  process.exit(0);
+    console.log('Data seeded!!')
+    process.exit(0);
 });
